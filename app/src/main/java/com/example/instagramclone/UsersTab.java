e4ae25c3f9803976382d6caae34ec65a7830b882
JoseDeAssis <jose.assis@aluno.uece.br>
2019-10-14 17:54:05 -0300

    finalizando projeto

--- app/src/main/java/com/example/instagramclone/UsersTab.java
+++ app/src/main/java/com/example/instagramclone/UsersTab.java
@@ -1,6 +1,7 @@
 package com.example.instagramclone;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -25,10 +28,10 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UsersTab extends Fragment {
+public class UsersTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     private ListView mListView;
-    private List mList;
+    private List<String> mList;
     private ArrayAdapter mArrayAdapter;
     private TextView mProgressTxt;
     private ProgressBar mProgressBar;
@@ -45,7 +48,9 @@ public class UsersTab extends Fragment {
         View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
 
         mListView = view.findViewById(R.id.listView);
-        mList = new ArrayList();
+        mListView.setOnItemClickListener(UsersTab.this);
+        mListView.setOnItemLongClickListener(UsersTab.this);
+        mList = new ArrayList<String>();
         mArrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, mList);
         mProgressBar = view.findViewById(R.id.progressBar);
         mProgressTxt = view.findViewById(R.id.progressTextView);
@@ -70,4 +75,27 @@ public class UsersTab extends Fragment {
         return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        Intent intent = new Intent(getContext(), UsersPosts.class);
+        intent.putExtra("username", mList.get(position));
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.whereEqualTo("username", mList.get(position));
+
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser object, ParseException e) {
+                if(object != null && e == null) {
+
+                }
+            }
+        });
+
+        return true;
+    }
 }
